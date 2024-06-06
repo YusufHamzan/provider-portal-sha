6dc38a97bd278b27188bfbfab1cf753d2121df8f
Rajiv <rajivkumars1027@gmail.com>
2024-06-06 17:11:24 +0530

    fix

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { styled } from "@mui/material/styles";
-import { Box, IconButton, Typography } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Typography,
+} from "@mui/material";
 import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -9,11 +16,21 @@ import { Avatar } from "primereact/avatar";
 import { jwtDecode } from "jwt-decode";
 import { useKeycloak } from "@react-keycloak/web";
 import { Button } from "primereact/button";
+import { Logout } from "@mui/icons-material";
 
 const Header = ({ open, handleDrawerOpen }) => {
   const { keycloak } = useKeycloak();
   let token = window["getToken"] && window["getToken"]();
   const { name } = jwtDecode(token);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const openSelect = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const AppBar = styled(MuiAppBar, {
     shouldForwardProp: (prop) => prop !== "open",
@@ -54,25 +71,93 @@ const Header = ({ open, handleDrawerOpen }) => {
           EOxegen
         </Typography>
         <Avatar icon="pi pi-user" size="normal" shape="circle" /> */}
-        <Box>
-          <img src={clientLogo} alt="" />
-        </Box>{" "}
-        {/* <Typography
-          variant="body1"
-          component="a"
-          sx={{ flexGrow: 1, marginleft: 4, cursor: "pointer" }}
+        <Box
+          display={"flex"}
+          justifyContent={"space-between"}
+          width={"100%"}
+          alignItems={"center"}
         >
-          {name}
-        </Typography> */}
-        <Button
-          onClick={() => {
-            localStorage.clear();
-            keycloak.logout();
-          }}
-        >
-          <i className="pi pi-sign-out" style={{ marginRight: "0.5rem" }}></i>
-          Logout
-        </Button>
+          <Box style={{ width: "300px", height: "55px" }}>
+            <img src={"/icons/sha_logo.svg"} alt="" />
+          </Box>
+          <Box
+            display={"flex"}
+            justifyContent={"space-between"}
+            width={"10%"}
+            alignItems={"center"}
+          >
+            <Typography
+              variant="body1"
+              component="a"
+              sx={{ flexGrow: 1, marginleft: 4 }}
+            >
+              {name}
+            </Typography>
+            <Box sx={{ marginLeft: "2%" }}>
+              <Logout
+              cursor={"pointer"}
+                fontSize="small"
+                onClick={() => {
+                  localStorage.clear();
+                  keycloak.logout();
+                }}
+              />
+            </Box>
+          </Box>
+          {/* <Button
+            onClick={() => {
+              localStorage.clear();
+              keycloak.logout();
+            }}
+          >
+            <i className="pi pi-sign-out" style={{ marginRight: "0.5rem" }}></i>
+            Logout
+          </Button> */}
+          {/* <Menu
+            anchorEl={anchorEl}
+            id="account-menu"
+            open={openSelect}
+            onClose={handleClose}
+            onClick={handleClose}
+            slotProps={{
+              paper: {
+                elevation: 0,
+                sx: {
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1.5,
+                  "& .MuiAvatar-root": {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  "&::before": {
+                    content: '""',
+                    display: "block",
+                    position: "absolute",
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: "background.paper",
+                    transform: "translateY(-50%) rotate(45deg)",
+                    zIndex: 0,
+                  },
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          >
+            <MenuItem>
+              <ListItemIcon>
+                <Logout fontSize="small" />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu> */}
+        </Box>
       </Toolbar>
     </AppBar>
   );
