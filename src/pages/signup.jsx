47b009953d253b801b7402804a52eb4102e92df7
sagarvivek17 <vivek.sagar@eoxegen.com>
2024-06-16 15:33:08 +0530

    did some chnages

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ProviderDetails from "../components/signup/provider.details.component";
 import { Box, Paper } from "@mui/material";
 
-const style={
-  background:"#313c96",
-  color:"#f1f1f1",
-  display:"flex",
-  justifyContent:"center",
-  height:"50px",
-  fontSize:"36px"
-}
+const style = {
+  background: "#313c96",
+  color: "#f1f1f1",
+  display: "flex",
+  justifyContent: "center",
+  height: "50px",
+  fontSize: "36px",
+};
 const Signup = () => {
   return (
     <Box>
