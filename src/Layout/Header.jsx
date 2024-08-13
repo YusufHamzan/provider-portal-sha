1748cf1974302c9cc0ae41bb15e352891e0e8fee
Rajiv <rajivkumars1027@gmail.com>
2024-08-13 16:24:28 +0530

    logo updated

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -43,10 +43,10 @@ const Header = ({ open, handleDrawerOpen }) => {
             width={"100%"}
             alignItems={"center"}
           >
-              <Typography sx={{fontSize:"24px", fontFamily:"monospace", color:"#000", fontWeight:"bold"}}>TORUS</Typography>
+              {/* <Typography sx={{fontSize:"24px", fontFamily:"monospace", color:"#000", fontWeight:"bold"}}>TORUS</Typography> */}
             <Box sx={{ flexGrow: 1 }} >
               {/* <img style={{ height: "50px" }} src={"https://aar-insurance.com/media/2023/05/aar-insurance-high-res-logo.png"} alt="" /> */}
-              {/* <img style={{ width: "300px", height: "50px" }} src={"/icons/sha_logo.svg"} alt="" /> */}
+              <img style={{ width: "300px", height: "50px" }} src={"/icons/sha_logo.svg"} alt="" />
             </Box>
             <Box sx={{ flexGrow: 0 }}>
               <ProfileSection logout={() => keycloak.logout()} />
