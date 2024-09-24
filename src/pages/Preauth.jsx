6ccf8b4387772865dfaa8f531b194d766fdb046f
Rajiv <rajivkumars1027@gmail.com>
2024-09-24 23:18:03 +0530

    ipd word remove

--- src/pages/Preauth.jsx
+++ src/pages/Preauth.jsx
@@ -6,12 +6,12 @@ const Preauth = () => {
   return (
     // <h1>Claims</h1>
     <TabView scrollable style={{ fontSize: "14px" }}>
-      <TabPanel leftIcon="pi pi-user mr-2" header="IPD Pre-Auth">
+      <TabPanel leftIcon="pi pi-user mr-2" header="Pre-Auth">
         <PreAuthIPDListComponent />
       </TabPanel>
-      <TabPanel leftIcon="pi pi-user-minus mr-2" header="OPD Pre-Auth">
+      {/* <TabPanel leftIcon="pi pi-user-minus mr-2" header="OPD Pre-Auth">
         <PreAuthOPDListComponent />
-      </TabPanel>
+      </TabPanel> */}
     </TabView>
   );
 };
