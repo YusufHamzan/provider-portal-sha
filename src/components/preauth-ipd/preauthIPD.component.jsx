3b4dad75ec1c3a40b9d86af9c68bedd99a2c7195
Imran Tarafder <imrantarafder@gmail.com>
2024-09-25 15:37:59 +0530

    subject id  change

--- src/components/preauth-ipd/preauthIPD.component.jsx
+++ src/components/preauth-ipd/preauthIPD.component.jsx
@@ -384,9 +384,8 @@ export default function ClaimsPreAuthIPDComponent(props) {
     let X = benefits?.forEach((ele) => {
       const parentBenefitName = benefitLookup[ele.parentBenefitStructureId];
       let obj = {
-        label: `${
-          parentBenefitName != undefined ? `${parentBenefitName} >` : ""
-        } ${ele.name}`,
+        label: `${parentBenefitName != undefined ? `${parentBenefitName} >` : ""
+          } ${ele.name}`,
         name: ele.name,
         value: ele.id,
         benefitStructureId: ele.benefitStructureId,
@@ -888,7 +887,7 @@ export default function ClaimsPreAuthIPDComponent(props) {
     }
   };
 
-  const matchResult = (result) => {};
+  const matchResult = (result) => { };
 
   const handleInterventionValidation = (val, i) => {
     const serviceDetailsListValid = serviceDetailsList
@@ -1036,7 +1035,7 @@ export default function ClaimsPreAuthIPDComponent(props) {
   const handleInitiate = () => {
     setBiometricInitiated(true);
     const payload = {
-      subject_id_number: "26263348",
+      subject_id_number: "31746114",
       relying_party_agent_id_number: "27759855",
       notification_callback_url:
         "https://shaapi.eo2cloud.com/member-command-service/v1/public/sha-member/biometric/callback",
@@ -1207,7 +1206,7 @@ export default function ClaimsPreAuthIPDComponent(props) {
 
                     <DialogContent>
                       {memberName?.res?.content &&
-                      memberName?.res?.content?.length > 0 ? (
+                        memberName?.res?.content?.length > 0 ? (
                         <TableContainer>
                           <Table>
                             <TableHead>
