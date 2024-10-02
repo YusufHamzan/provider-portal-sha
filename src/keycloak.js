6d3941ad30d71ed4a4b1eadab2932ece8d726795
Imran Tarafder <imrantarafder@gmail.com>
2024-10-02 08:48:29 +0530

    config only update

--- src/keycloak.js
+++ src/keycloak.js
@@ -3,6 +3,7 @@ import Keycloak from 'keycloak-js';
 const keycloak = new Keycloak({
   url: 'https://shaidentity.eo2cloud.com',
   // url: "https://identity.eoxegen.com/",
+  // url: "https://identity.sha.go.ke",
   realm: "eo2v2",
   clientId: "eo2v2-provider-web-client",
 });
--- src/remote-api/configuration.ts
+++ src/remote-api/configuration.ts
@@ -1,4 +1,5 @@
 export const config = {
 	// rootUrl: 'https://api.eoxegen.com',
 	rootUrl: 'https://shaapi.eo2cloud.com',
+	// rootUrl: 'https://api.sha.go.ke',s
 };
