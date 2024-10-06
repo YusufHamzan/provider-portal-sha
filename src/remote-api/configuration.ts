56dee238b48ce08834a7c139418c4127677c8f8e
Imran Tarafder <imrantarafder@gmail.com>
2024-10-06 13:53:36 +0530

    api

--- src/remote-api/configuration.ts
+++ src/remote-api/configuration.ts
@@ -1,5 +1,5 @@
 export const config = {
 	// rootUrl: 'https://api.eoxegen.com',
-	rootUrl: 'https://shaapi.eo2cloud.com',
+	rootUrl: 'https://shaapi.eo2cloud.coma',
 	// rootUrl: 'https://api.sha.go.ke',
 };
